8881a70df3733109ae2f7573da699d48ad8a1645
devatharshini1999 <31559005+devatharshini1999@users.noreply.github.com>
2017-09-02 15:40:49 +0530

    Create count.c

---
+++ count.c
@@ -0,0 +1,19 @@
+
+
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n,r,i,count=0;
+    scanf("%d",&n);
+while(n>0)   
+{
+      r=n%10;  
+        count++;
+    
+    n=n/10;
+}
+    printf("%d",count);
+}
+    
