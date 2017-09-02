ba5eb2743a5a21e20abd0054b982b932f4c57e4c
devatharshini1999 <31559005+devatharshini1999@users.noreply.github.com>
2017-09-02 12:47:13 +0530

    Update and rename vowels_cons.c to character.c

---
+++ character.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char c;
+scanf("%c",&c);
+if((c='a' || c='e' || c='i' || c='o' c='u') && (c='A' || c='E' || c='I' || c='O' || c='U'))
+{
+printf("character is vowel %c",c);
+}
+else
+{
+printf("character is consonant %c",c);
+}
+}
