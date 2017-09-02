c5e895c0b14cf989364c5485cf7c2e39a7ecae63
devatharshini1999 <31559005+devatharshini1999@users.noreply.github.com>
2017-09-02 14:57:22 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -2,9 +2,17 @@
 #include<conio.h>
 void main()
 {
-int i;
-for(i=0;i<5;i++)
+int i,n;
+scanf("%d",&n);
+if(n>=0)
+{
+for(i=0;i<n;i++)
 {
     printf("Hello\n");
+}
+}
+else
+{
+    printf("invalid");
 }
 }
